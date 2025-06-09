189d183605c43ffbd71d1363433c075af37f3d2d
YeruhamMendelson <y0583207549@gmail.com>
2025-06-09 13:03:28 +0300

    Add project files.

---
+++ malsinon.sql
@@ -0,0 +1,105 @@
+CREATE DATABASE IF NOT EXISTS malsinon;
+USE malsinon;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 09, 2025 at 11:43 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `malsinon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `intelreports`
+--
+
+CREATE TABLE `intelreports` (
+  `id` int(11) NOT NULL,
+  `reporter_id` int(11) NOT NULL,
+  `target_id` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `timestamp` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `people`
+--
+
+CREATE TABLE `people` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `secret_code` varchar(20) NOT NULL,
+  `type` enum('reporter','target','both','potential_agent') NOT NULL,
+  `num_reports` int(11) DEFAULT 0,
+  `num_mentions` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `intelreports`
+--
+ALTER TABLE `intelreports`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `reporter_id` (`reporter_id`),
+  ADD KEY `target_id` (`target_id`);
+
+--
+-- Indexes for table `people`
+--
+ALTER TABLE `people`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `secret_code` (`secret_code`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `intelreports`
+--
+ALTER TABLE `intelreports`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `people`
+--
+ALTER TABLE `people`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `intelreports`
+--
+ALTER TABLE `intelreports`
+  ADD CONSTRAINT `intelreports_ibfk_1` FOREIGN KEY (`reporter_id`) REFERENCES `people` (`id`),
+  ADD CONSTRAINT `intelreports_ibfk_2` FOREIGN KEY (`target_id`) REFERENCES `people` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
